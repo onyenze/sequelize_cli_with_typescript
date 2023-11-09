8797b3412d61296f23d7eea63157814963c8291b
onyenze <chibuezeonyenze123@gmail.com>
2023-11-09 03:36:49 +0100

    tests

--- models/index.ts
+++ models/index.ts
@@ -11,10 +11,6 @@ const db: any = {};
 
 
 
-// __filename);
-// const env = process.env.NODE_ENV || 'development';
-// const config = require(__dirname + '/../config/config.js')[env];
-// const db: any = {};
 let sequelize: any;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
